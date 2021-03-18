148ed878d21357b0fcb98ddf61f0a52a91e03ab0
sgosiaco <sgosiaco@me.com>
2021-03-18 02:46:25 -0700

    Updated VPN class. Added VPNList widget. Default sorted the list by ping. Replaced future builder to prevent future from getting called again while list is being rebuilt after filter. Added buttons to filter by server region.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:convert';
+import 'dart:html' as html;
 import 'package:http/http.dart' as http;
+import 'package:csv/csv.dart';
+
 
 void main() {
   runApp(App());
@@ -31,6 +33,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<VPN> _data = [];
+  List<VPN> _view = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getVPNList().then(
+      (data) {
+        setState(() {
+          _data = data;
+          _view = data;
+        });
+      }
+    ).onError((error, stackTrace) {
+      setState(() {
+        _data = null;
+      });
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,29 +60,55 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder(
-        future: getVPNList(),
-        builder: (BuildContext context, AsyncSnapshot<List<VPN>> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data.length * 2,
-              itemBuilder: (context, index) {
-                if (index.isOdd) return Divider();
-
-                final idx = index ~/ 2;
-
-                return ListTile(
-                  title: Text(snapshot.data[idx].hostname),
-                  subtitle: Text('${snapshot.data[idx].countryShort} | ${snapshot.data[idx].ping} | ${snapshot.data[idx].speed}'),
-                );
-              }
+      body: Builder(
+        builder: (context) {
+          if (_data != []) {
+            return Column(
+              children: [
+                ButtonBar(
+                  alignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(child: Text('Canada'), onPressed: () {
+                      setState(() {
+                        _view = _data.where((element) => element.countryShort == 'CA').toList();
+                      });
+                    }),
+                    TextButton(child: Text('Japan'), onPressed: () {
+                      setState(() {
+                        _view = _data.where((element) => element.countryShort == 'JP').toList();
+                      });
+                    }),
+                    TextButton(child: Text('Korea'), onPressed: () {
+                      setState(() {
+                        _view = _data.where((element) => element.countryShort == 'KR').toList();
+                      });
+                    }),
+                    TextButton(child: Text('United States'), onPressed: () {
+                      setState(() {
+                        _view = _data.where((element) => element.countryShort == 'US').toList();
+                      });
+                    }),
+                    TextButton(child: Text('Vietnam'), onPressed: () {
+                      setState(() {
+                        _view = _data.where((element) => element.countryShort == 'VN').toList();
+                      });
+                    }),
+                    TextButton(child: Text('Reset'), onPressed: () {
+                      setState(() {
+                        _view = _data;
+                      });
+                    })
+                  ],
+                ),
+                Expanded(child: VPNList(data: _view))
+              ]
             );
-          } else if (snapshot.hasError) {
+          } else if (_data == null) {
             return Center(child: Text('Couldn\'t load list from VPN Gate. Please refresh!'),);
           } else {
             return Center(child: CircularProgressIndicator(),);
           }
-        }
+        },
       )
     );
   }
@@ -72,13 +120,40 @@ class _HomePageState extends State<HomePage> {
     if (res.statusCode == 200) {
       List<List<dynamic>> csv = const CsvToListConverter().convert(res.body);
       print('Raw CSV length: ${csv.length}');
-      return csv.sublist(2, csv.length - 1).map<VPN>((raw) => VPN.fromList(raw)).toList();
+      return csv.sublist(2, csv.length - 1).map<VPN>((raw) => VPN.fromList(raw)).toList()..sort((a, b) => a.ping - b.ping);
     } else {
       print('Error!');
       return Future.error('Failed to load VPN list!');
     }
   }
+}
 
+class VPNList extends StatelessWidget {
+  final List<VPN> data;
+
+  VPNList({this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: data.length * 2,
+      itemBuilder: (context, index) {
+        if (index.isOdd) return Divider();
+
+          final idx = index ~/ 2;
+
+          return ListTile(
+            title: Text('${data[idx].hostname} | ${data[idx].ip}'),
+            subtitle: Text('${data[idx].countryShort} | ${data[idx].ping} ms | ${data[idx].speed.toStringAsFixed(2)} Mbps | ${data[idx].uptime.toStringAsFixed(0)} days | ${data[idx].totalTraffic.toStringAsFixed(2)} GB'),
+            trailing: Text('${data[idx].score}'),
+            onTap: () {
+              html.window.open(html.Url.createObjectUrlFromBlob(html.Blob([utf8.decode(base64Decode(data[idx].openVPN64))], 'text/plan', 'native')), "text");
+            },
+          );
+        }
+    );
+  }
 }
 
 class VPN {
@@ -87,13 +162,13 @@ class VPN {
   final String ip;
   final int score;
   final int ping;
-  final int speed;
+  final double speed;
   final String countryLong;
   final String countryShort;
   final int numVPNSessions;
-  final int uptime;
+  final double uptime;
   final int totalUsers;
-  final int totalTraffic;
+  final double totalTraffic;
   final String logType;
   final String op;
   final String message;
@@ -106,13 +181,13 @@ class VPN {
     ip: raw[1],
     score: raw[2],
     ping: raw[3],
-    speed: raw[4],
+    speed: raw[4] / 1000000, // in bps so divide by 1mil to get Mbps
     countryLong: raw[5],
-    countryShort: raw[6],
+    countryShort: raw[6].toUpperCase(),
     numVPNSessions: raw[7],
-    uptime: raw[8],
+    uptime: raw[8] / (1000*60*60*24), // in ms so divide by 1000 60 60 24
     totalUsers: raw[9],
-    totalTraffic: raw[10],
+    totalTraffic: raw[10] / 1000000000, // in bytes so divide by 1bil to get GB
     logType: raw[11],
     op: raw[12],
     message: raw[13],
